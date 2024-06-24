08bb299c0d816dffd1c0cad44dc1e9a0fcfdf87b
jaennil <jaennil@yandex.ru>
2024-06-24 04:15:34 +0300

    feat(cat-file): check edge cases; prevent zipbombs

--- git/src/main.rs
+++ git/src/main.rs
@@ -1,6 +1,7 @@
 use std::{
+    ffi::CStr,
     fs::{self, File},
-    io::Read,
+    io::{BufRead as _, BufReader, Read, Write as _},
     path::PathBuf,
 };
 
@@ -53,13 +54,43 @@ fn main() -> anyhow::Result<()> {
                     .collect();
             let object_file =
                 File::open(object_path).context("can't find object file at {object_path}")?;
-            let mut zlib_decoder = ZlibDecoder::new(object_file);
-            let mut string_buffer = String::new();
-            zlib_decoder
-                .read_to_string(&mut string_buffer)
-                .context("data in object is not valid UTF-8")?;
-            let result: String = string_buffer.split('\0').skip(1).collect();
-            println!("{result}");
+            let zlib_decoder = ZlibDecoder::new(object_file);
+            let mut zlib_decoder_reader = BufReader::new(zlib_decoder);
+            let mut buffer: Vec<u8> = Vec::new();
+            zlib_decoder_reader
+                .read_until(0, &mut buffer)
+                .context("read header of {object_path}")?;
+            let header = CStr::from_bytes_with_nul(&buffer)
+                .expect("know there is exactly on nul, and it's at the end");
+            let header = header
+                .to_str()
+                .context("object header is not valid UTF-8")?;
+            let Some((kind, size)) = header.split_once(' ') else {
+                anyhow::bail!(
+                    "object file header must contain space delimiting object type and size"
+                );
+            };
+            let size = size
+                .parse::<usize>()
+                .context("object file has invalid size: {size}")?;
+            match kind {
+                "blob" => {
+                    buffer.clear();
+                    buffer.resize(size, 0);
+                    zlib_decoder_reader
+                        .read_exact(&mut buffer)
+                        .context("read contents of the object")?;
+                    let left_unread = zlib_decoder_reader
+                        .read(&mut [0])
+                        .context("validate EOF in object")?;
+                    anyhow::ensure!(left_unread == 0, "object file has trailing bytes");
+                    let mut stdout = std::io::stdout();
+                    stdout
+                        .write_all(&buffer)
+                        .context("write object contents to stdout")?;
+                }
+                _ => anyhow::bail!("can't print {kind} yet"),
+            }
         }
     }
 
